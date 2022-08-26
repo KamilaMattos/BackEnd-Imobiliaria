559090fafa4e6ecce909462802b2ff5015e1fa00
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-08-26 17:53:43 -0300

    controllers and services structure created

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,23 @@ import "reflect-metadata"
 import "express-async-errors"
 import express from "express"
 
+import { usersRoute } from "./routes/users"
+import { loginRoute } from "./routes/login"
+import { categoriesRoute } from "./routes/categories"
+import { propertiesRoute } from "./routes/properties"
+import { schedulesRoute } from "./routes/schedules/index.ts"
+
+import { handleError } from "./middlewares/errors.middleware"
+
 const app = express()
 app.use(express.json())
 
+app.use("/users", usersRoute)
+app.use("/login", loginRoute)
+app.use("/categories", categoriesRoute)
+app.use("/properties", propertiesRoute)
+app.use("/schedules", schedulesRoute)
+
+app.use(handleError)
 
 export default app
